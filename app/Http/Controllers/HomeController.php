9c4d9cc280fdbd72e8aeabcd2efbd72232e6c136
MeliskaYI <meliskayaffaivana725myip29@gmail.com>
2021-03-05 22:19:11 +0700

    Jobsheet 3 Kelompok

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        echo "Welcome";
+        return view('Home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\AboutController;
-use App\Http\Controllers\ArticleController;
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/about', [AboutController::class, 'about']); 
-Route::get('articles/{id}' , [ArticleController::class, 'articles']) ; 
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
 
 
 
