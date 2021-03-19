3e1e429383e6f4dcfea6e40683c38abddb43870f
MeliskaYI <meliskayaffaivana725myip29@gmail.com>
2021-03-19 10:10:05 +0700

    Jobsheet 6 Practicum 2 : Add login as Administrator

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\IndexController;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\ContactController;
 
-Route::get('/', [HomeController::class, 'home']);
+Route::get('/', [IndexController::class, 'index']);
 
 Route::get('menu', function(){
     $menu = DB::table('menu')->get();
@@ -18,3 +18,7 @@ Route::get('contact', function(){
 
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
